2f80779671c15c1ed5c7782704e595c4782d721e
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-11-07 15:24:40 +0530

    chore: handle navigation

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,11 +6,20 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0, // this will be set when a new tab is tapped
+        onTap: onTabTapped,
+        currentIndex: _currentIndex, // this will be set when a new tab is tapped
         items: [
           BottomNavigationBarItem(
             icon: new Icon(Icons.label),
